62769868e22c7637586b8c053dc7ed1ff5d2b743
Sujan Malakar <smsbmalakar@gmail.com>
2017-03-18 14:13:19 +0545

    field added for student table and authentication created

--- database/migrations/2017_03_18_081533_create_students_table.php
+++ database/migrations/2017_03_18_081533_create_students_table.php
@@ -15,6 +15,10 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('school');
             $table->timestamps();
         });
     }
